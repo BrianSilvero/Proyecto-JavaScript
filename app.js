2c263a83dc37e535ef19cc3f2c2330bbc7ddcaf5
Brian Silvero <briandavidsilvero@gmail.com>
2023-07-11 23:23:40 -0300

    2do commit

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-perrito= prompt("Dale don?")
+// Creando carrito para tienda de pagina de recetas online
+let resultado = 0;
+let ticket = "Resumen de la cuenta: \n";
+let rta = "";
 
-alert("Dale don? "+perrito).toUpperCase
+function iva(precio){
+    return precio * 0.21
+}
+do {
+  let producto = prompt("Ingrese el articulo a comprar").toLowerCase();
+  let precio = Number(prompt("Ingrese precio del producto"));
+  resultado = resultado + precio + iva(precio);
+  ticket = ticket + "\n" + producto + "\t$" + (precio + iva(precio));
+
+  rta = prompt(
+    "Quisiera agregar algo mas al carrito? (Escriba 'Esc' para finalizar compra)"
+  ).toUpperCase();
+} while (rta != "ESC");
+
+alert(ticket + "\n\nTotal: " + resultado)
